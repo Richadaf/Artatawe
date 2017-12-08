6dd3e2e281aa6d52fa1ae9147394ad4b95543c88
sidgrg <sidgurung97@gmail.com>
2017-12-08 17:53:30 +0000

    Browsing class started

---
+++ src/users/Browsing.java
@@ -0,0 +1,23 @@
+package users;
+
+/**
+ * The Browsing class is responsible for holding all the necessary behaviours
+ * needed for the right functionality of the Browsing GUI.
+ * @author Siddhartha Gurung (912318)
+ */
+
+public class Browsing {
+
+  public Browsing () {}
+
+  /**
+  * Marks a seller as a favourite for the logged in user
+  * @param favUser The seller which the user wants to favourite
+  * @return {@code true} if the seller has been successfully added as a
+  *        'favourite';
+  *        {@code false} otherwise
+ 	*/
+  public boolean markFavourite (User favUser) {
+      
+  }
+}
