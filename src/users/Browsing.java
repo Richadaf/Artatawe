524b1d90446ef4711139e0d14d83417066287a2d
sidgrg <sidgurung97@gmail.com>
2017-12-08 20:23:45 +0000

    markFavorite method updated

--- src/users/Browsing.java
+++ src/users/Browsing.java
@@ -7,17 +7,24 @@ package users;
  */
 
 public class Browsing {
+    private User currentUser;
 
-  public Browsing () {}
+    public Browsing() {}
 
-  /**
-  * Marks a seller as a favourite for the logged in user
-  * @param favUser The seller which the user wants to favourite
-  * @return {@code true} if the seller has been successfully added as a
-  *        'favourite';
-  *        {@code false} otherwise
- 	*/
-  public boolean markFavourite (User favUser) {
-      
-  }
+    /**
+     * Marks a seller as a favourite for the logged in user
+     * @param favUser The seller which the user wants to favourite
+     * @return {@code true} if the seller has been successfully added as a
+     *        'favourite';
+     *        {@code false} otherwise
+     */
+    public boolean markFavorite(User favUser) {
+        for (int i=0; i < currentUser.getFavoriteUsers().size(); i++) {
+            if (currentUser.getFavoriteUsers().get(i).getUserName().equals(favUser.getUserName())) {
+              return false;
+            }
+        }
+        currentUser.addFavoriteUser(favUser);
+        return true;
+    }
 }
